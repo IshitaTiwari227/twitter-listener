3e35a264e22e236ce35e606255e618804b455f77
IshitaTiwari227 <Ishita_tiwari@condenast.com>
2021-08-03 19:45:41 +0530

    embedding tweets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage/MainPage";
-import request from "request-promise";
-import { useEffect } from "react";
-
-const host = "http://localhost:5000";
 
 function App() {
-  const getTweets = async (hashtag) => {
-    const body = await request.get(`${host}/tweets/${hashtag}`);
-    const result = JSON.parse(body);
-    return result;
-  };
-
-  useEffect(() => {
-    getTweets("iamatraveler").then((data) => {
-      console.log(data);
-    });
-  }, []);
-
   return (
     <div className="App">
       <MainPage />
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import withStyles from "react-jss";
-import SearchBar from "../SearchBar/SearchBar";
+import request from "request-promise";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
+
+const host = "http://localhost:5000";
+
 //import searchIcon from "../../Assets/searchIcon.png";
 
 const styles = {
+  mainContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
   leftContainer: {
     width: "30%",
     display: "flex",
@@ -12,6 +31,12 @@ const styles = {
     padding: "70px 20px 0px 40px",
     background: "#F9F9F9",
     height: "100vh",
+  },
+  rightContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "70%",
   },
   title: {
     fontStyle: "normal",
@@ -41,27 +66,85 @@ const styles = {
     fontWeight: "600",
     fontSize: "16px",
   },
+  searchBar: {
+    marginTop: "20px",
+    width: "25rem",
+    background: "#FFFFFF",
+    border: "1px solid #dddddd",
+    padding: "0.7rem",
+    color: "#979797",
+    fontFamily: "Rubik",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "16px",
+    height: "48px",
+    "@media only screen and (max-width: 600px) ": {
+      width: "19rem",
+      fontSize: "12px",
+    },
+  },
 };
 
 const MainPage = ({ classes }) => {
+  let [hashtag, sethashtag] = useState("");
+  let [tweets, setTweets] = useState([]);
+  let searchedtag = "";
+  let handleInput = (e) => {
+    searchedtag = e.target.value;
+  };
+
+  const submit = async (e) => {
+    sethashtag(searchedtag);
+  };
+
+  let getTweets = async (tag) => {
+    console.log(tag);
+    const body = tag ? await request.get(`${host}/tweets/${tag}`) : null;
+    const result = JSON.parse(body);
+    return result;
+  };
+
+  useEffect(() => {
+    getTweets(hashtag).then((data) => {
+      console.log(data);
+      if (data && data.statuses.length > 0) return setTweets(data.statuses);
+    });
+  }, [hashtag]);
+
   return (
-    <div className={classes.leftContainer}>
-      <div className={classes.title}>{"Twitter Listener"}</div>
-      <div>{"#hashtag"}</div>
-      <SearchBar />
-      <div className={classes.checkbox}>
-        <input
-          type="checkbox"
-          id={"include image"}
-          value={"include image"}
-          readOnly
-        ></input>
-        <label htmlFor={"include image"} className={classes.labelText}>
-          {"must include an image"}
-        </label>
+    <div className={classes.mainContainer}>
+      <div className={classes.leftContainer}>
+        <div className={classes.title}>{"Twitter Listener"}</div>
+        <div>{"#hashtag"}</div>
+        <div onChange={(e) => handleInput(e)}>
+          <input
+            className={classes.searchBar}
+            //value={keyword}
+          />
+        </div>
+        <div className={classes.checkbox}>
+          <input
+            type="checkbox"
+            id={"include image"}
+            value={"include image"}
+            readOnly
+          ></input>
+          <label htmlFor={"include image"} className={classes.labelText}>
+            {"must include an image"}
+          </label>
+        </div>
+        <div className={classes.submitButton}>
+          <div onClick={(e) => submit(e)} className={classes.buttonText}>
+            {"Submit"}
+          </div>
+        </div>
       </div>
-      <div className={classes.submitButton}>
-        <div className={classes.buttonText}>{"Submit"}</div>
+      <div className={classes.rightContainer}>
+        {tweets && tweets.length > 0
+          ? tweets.map((tweet) => {
+              return <TwitterTweetEmbed tweetId={tweet.id_str} />;
+            })
+          : null}
       </div>
     </div>
   );
